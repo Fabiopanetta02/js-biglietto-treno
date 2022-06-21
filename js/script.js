d465f6f0eb539a4570580f647d22070c6f324628
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-06-21 13:44:06 +0200

    Added p with id

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*Schema logico
+1- Recupero il paragrafo con l'id
+2- Chiedo il numero di chilometri
+3- Chiedo l'età del passeggero
+5- Creo la password mettendo insieme le informazioni
+6- Faccio comparire la password 
+*/
+
+//1- Recupero il paragrafo con l'id
+const resulParagraph = document.getElementById('price-ticket')
+console.log('price-ticket')
