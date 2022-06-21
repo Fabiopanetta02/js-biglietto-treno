446272b7fde9bf884a65970f779c41b1164d1ab1
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-06-21 15:42:14 +0200

    Added finish basic exercise

--- js/script.js
+++ js/script.js
@@ -2,14 +2,37 @@
 1- Recupero il paragrafo con l'id
 2- Chiedo il numero di chilometri
 3- Chiedo l'età del passeggero
-5- Creo la password mettendo insieme le informazioni
-6- Faccio comparire la password 
+4- Calcolo il prezzo totale del viaggio sulla base della tariffa
+5- Applico sconto  
 */
 
 //1- Recupero il paragrafo con l'id
-const resulParagraph = document.getElementById('price-ticket')
-console.log('price-ticket')
+const resulParagraph = document.getElementById('price-ticket');
+console.log('price-ticket');
 
 //2- Chiedo il numero di chilometri
-const numberchilo = prompt('Numero di chilometri da percorrere', '100km')
-console.log('numberchilo')
+const numberChilo = parseInt(prompt('Numero di chilometri da percorrere?', '100km'));
+console.log('numberChilo');
+
+//3- Chiedo l'età del passeggero
+const numberAge = prompt('Età del passeggero?', 30);
+console.log('numberAge');
+
+//4- Calcolo il prezzo totale del viaggio sulla base della tariffa
+let priceKm = numberChilo * 0.21;
+console.log('priceKm');
+
+//5- Applico sconto 
+if(numberAge < 18){
+    priceKm *= ( (100-20) / 100 );
+    console.log('priceDiscount')
+}
+else if(numberAge > 65){
+    priceKm *= ( (100-40) / 100 )
+    console.log('over65')
+}
+
+const pricePrinted = priceKm.toFixed(2)
+
+//6- Faccio comparire il prezzo
+resulParagraph.innerText =  `Il prezzo totale del biglietto é: ${pricePrinted} €`
