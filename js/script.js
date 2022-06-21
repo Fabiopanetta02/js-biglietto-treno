3b037a5d2c0299898c73b2e195de4fd1b88d171a
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-06-21 19:13:22 +0200

    Added validation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 2- Chiedo il numero di chilometri
 3- Chiedo l'età del passeggero
 4- Calcolo il prezzo totale del viaggio sulla base della tariffa
-5- Applico sconto  
+5- Applico sconto
+6-Creo una variabile da stampare
+7- Faccio comparire il prezzo
 */
 
 //1- Recupero il paragrafo con l'id
@@ -23,16 +25,24 @@ let priceKm = numberChilo * 0.21;
 console.log('priceKm');
 
 //5- Applico sconto 
-if(numberAge < 18){
-    priceKm *= ( (100-20) / 100 );
-    console.log('priceDiscount')
+
+// ! VALIDAZIONE
+if(isNaN(numberChilo) || isNaN(numberAge)){
+    alert('L\'età inserita non è valida')
 }
-else if(numberAge > 65){
-    priceKm *= ( (100-40) / 100 )
-    console.log('over65')
+else{
+    if(numberAge < 18){
+        priceKm *= ( (100-20) / 100 );
+        console.log('priceDiscount')
+    }
+    else if(numberAge > 65){
+        priceKm *= ( (100-40) / 100 )
+        console.log('over65')
+    }
 }
 
+//6-Creo una variabile da stampare
 const pricePrinted = priceKm.toFixed(2)
 
-//6- Faccio comparire il prezzo
+//7- Faccio comparire il prezzo
 resulParagraph.innerText =  `Il prezzo totale del biglietto é: ${pricePrinted} €`
